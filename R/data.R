834838e3940c0082bb6dc319ac84f02baf4fad64
Bak <bak@Bakui-MacBookPro.local>
2024-04-13 00:04:53 +0900

    data descrption

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' A polar wander dataset
+#'
+#' A polar wander dataset presented in Kent and Irving (2010). The 17 Triassic/Jurassic cratonic poles from other major cratons are rotated into North American coordinates and combined with the 14 observations from North America. Our method is applied to these 31 observations ranging in age from 243 to 144 Ma (millions of years ago), which covers the late Triassic and Jurassic periods. The first column represents the time points, and the remaining two columns provides the observed spherical coordinates.
+#'
+#' @name apw_spherical
+#' @docType data
+NULL
+#' A tropical cyclone dataset
+#'
+#' A tropical cyclone dataset provided by the Regional Specialized Meteorological Center (RSMC) Tokyo Typhoon Center. We select a cyclone called 'Goni' observed over time  on August, 2015. The first column represents the time points, and the remaining two columns provides the observed spherical coordinates.
+#'
+#' @name goni_spherical
+#' @docType data
+NULL
+
